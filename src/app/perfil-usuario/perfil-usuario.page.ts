ffcbe2140753d13b0338116265d0364c5aa43520
DonTaka <jo.riquelmees@gmail.com>
2023-08-18 10:31:52 -0400

    Update perfil-usuario.page.ts

--- src/app/perfil-usuario/perfil-usuario.page.ts
+++ src/app/perfil-usuario/perfil-usuario.page.ts
@@ -9,16 +9,7 @@ import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
 export class PerfilUsuarioPage implements OnInit {
 
   constructor(private router: Router, private activeRoute: ActivatedRoute) {
-    var user = {
-      username: "",
-      password: ""
-    }
-    this.activeRoute.queryParams.subscribe(params => {
-      if (this.router.getCurrentNavigation()?.extras.state) {
-        user = this.router.getCurrentNavigation().extras.state.user;
-        console.log(user);
-      }
-    })
+
   }
 
   ngOnInit() {
